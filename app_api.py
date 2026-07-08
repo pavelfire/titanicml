3d6443415cdeae052721c4555feed9f42c89c27c
pvdo <fire_ra@bk.ru>
2026-07-08 10:08:19 +0300

    fix error, now can run

--- app_api.py
+++ app_api.py
@@ -3,23 +3,48 @@
 вторая для получения количества сделанных запросов (/stats), и третья для проверки работы API (/health).
 
 Шаг 1: Установка необходимых библиотек
-pip install fastapi uvicorn pydantic scikit-learn pandas
+source .venv/bin/activate
+pip3 install fastapi uvicorn pydantic scikit-learn pandas
+pip3 install matplotlib   
 
 Шаг 2: Создание app_api.py
-Шаг 3: Запустите ваше приложение: python app_api.py
+Шаг 3: Запустите ваше приложение:
+
+В терминале перейдите в папку проекта и выполните:
+
+cd /Users/pvdo/VScodeProjects/titanicml
+source .venv/bin/activate
+После активации в начале строки терминала появится (.venv):
+
+(.venv) pvdo@MacBook-Pro-Pavel titanicml %
+Теперь команды python и pip будут из виртуального окружения:
+
+pip install fastapi uvicorn pydantic scikit-learn pandas
+python app_api.py
+Деактивация
+deactivate
+Если .venv ещё нет
+Создайте и сразу активируйте:
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+ python3 app_api.py
 Шаг 4: Тестирование API
 Теперь вы можете тестировать ваше API с помощью curl или любого другого инструмента для отправки HTTP запросов.
 
 curl -X GET http://127.0.0.1:5000/health
 curl -X GET http://127.0.0.1:5000/stats
-curl -X POST http://127.0.0.1:5000/predict_model -H "Content-Type: application/json" -d "{"Pclass": 3, "Age": 22, "Fare": 7.25}"
+curl -X POST http://127.0.0.1:5000/predict_model \
+  -H "Content-Type: application/json" \
+  -d '{"Pclass": 3, "Age": 22, "Fare": 7.25}'
 '''
 
 from fastapi import FastAPI, Request, HTTPException
 import pickle
 import pandas as pd
 from pydantic import BaseModel
-from train import predictions
+# from train import predictions
 
 app = FastAPI()
 
@@ -45,22 +70,22 @@ def health():
     return {"status": "ok"}
 
 @app.post("/predict_model")
-def predict_model(input_data: PredictInput):
+def predict_model(input_data: PredictionInput):
     global request_count
     request_count += 1
 
     #Создание DataFrame из данных
     new_data = pd.DataFrame({
         'Pclass': [input_data.Pclass],
-        'Aga': [input_data.Age],
+        'Age': [input_data.Age],
         'Fare': [input_data.Fare]
     })
 
     #Предсказание
-    prediction = model.prdict(new_data)
+    prediction = model.predict(new_data)
 
     #Преобразование результата в человеко-читаемый формат
-    result = "Survived" if predictions[0] == 1 else "Not Survived"
+    result = "Survived" if prediction[0] == 1 else "Not Survived"
 
     return {"prediction": result}
 
