1517deb28dc837a3183c8c446465bc01b223ed52
pvdo <fire_ra@bk.ru>
2026-07-08 12:41:58 +0300

    0.0.0.0 for run in docker now all works good

--- app_api.py
+++ app_api.py
@@ -91,6 +91,6 @@ def predict_model(input_data: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5026)
+    uvicorn.run(app, host="0.0.0.0", port=5026)
 
     
